0b7a50d0e3f13d04da0c6ed71395ca7f194067dc
IDerkBot <ivanderk2003@gmail.com>
2022-03-06 22:00:23 +0300

    Страница регистрации

---
+++ Pages/RegPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using ScheduleWPF.Classes;
+using ScheduleWPF.Entity;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ScheduleWPF.Pages
+{
+	/// <summary>
+	/// Логика взаимодействия для RegPage.xaml
+	/// </summary>
+	public partial class RegPage : Page
+	{
+		public RegPage() => InitializeComponent();
+
+		private void BtnReg_Click(object sender, RoutedEventArgs e)
+		{
+			var login = TbLogin.Text;
+			var password = PbPassword.Password;
+			if (!ARMEntities.GetContext().Users.Any(x => x.Login == login))
+			{
+				ARMEntities.GetContext().Users.Add(new User(login, password));
+				ARMEntities.GetContext().SaveChanges();
+				MessageBox.Show("Вы успешно зарегистрировались!");
+				Manager.GoBack();
+			}
+			else
+				MessageBox.Show("Такой пользователь уже существует!");
+		}
+	}
+}
