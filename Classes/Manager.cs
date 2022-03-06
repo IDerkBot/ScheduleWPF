564b53d9cef5104a00d69ffe05c16f622707e390
IDerkBot <ivanderk2003@gmail.com>
2022-03-06 21:58:41 +0300

    Создал класс Manager для работы со страницами

---
+++ Classes/Manager.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+
+namespace ScheduleWPF.Classes
+{
+	internal class Manager
+	{
+		public static Frame MainFrame { get; set; }
+		public static void Navigate(Page moveToPage) => MainFrame.Navigate(moveToPage);
+		public static void GoBack()
+		{
+			if(MainFrame.CanGoBack) MainFrame.GoBack();
+		}
+	}
+}
