1f3d5f33c088bb101449d6cc183cd908cd56b4b4
IDerkBot <ivanderk2003@gmail.com>
2022-03-06 22:00:04 +0300

    Страница Авторизации

---
+++ Pages/AuthPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using ScheduleWPF.Classes;
+using ScheduleWPF.Entity;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ScheduleWPF.Pages
+{
+	/// <summary>
+	/// Логика взаимодействия для AuthPage.xaml
+	/// </summary>
+	public partial class AuthPage : Page
+	{
+		public AuthPage()
+		{
+			InitializeComponent();
+		}
+
+		private void BtnAuth_Click(object sender, RoutedEventArgs e)
+		{
+			if(string.IsNullOrWhiteSpace(TbLogin.Text) || string.IsNullOrWhiteSpace(PbPassword.Password)) return;
+			if (Authorization.HaveUser(TbLogin.Text))
+			{
+				if (Authorization.CheckPassword(TbLogin.Text, PbPassword.Password))
+				{
+					var user = Authorization.GetUser(TbLogin.Text);
+					Data.IDUser = user.ID;
+					Data.Access = user.Access;
+					Manager.Navigate(new Menu());
+				}
+				else
+					MessageBox.Show("Пароль не верный!");
+			}
+			else
+				MessageBox.Show("Пользователь не найден!");
+		}
+
+		private void BtnRegMove_Click(object sender, RoutedEventArgs e)
+		{
+			Manager.Navigate(new RegPage());
+		}
+	}
+}
