08652f9537c5e1fc3d12c90a4971c6486b2b0ec6
IDerkBot <ivanderk2003@gmail.com>
2022-03-06 21:59:07 +0300

    Создал класс даннхы для удобной работы с данными на всем проекта

---
+++ Classes/Data.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScheduleWPF.Classes
+{
+	internal class Data
+	{
+		public static int IDUser { get; set; }
+		public static int Access { get; set; }
+		public static bool IsStudent => Access == 0;
+		public static bool IsTeacher => Access == 1;
+		public static bool IsLessonWork => Access == 2;
+		public static bool IsEducationWork => Access == 3;
+		public static bool IsAdmin => Access == 4;
+	}
+}
