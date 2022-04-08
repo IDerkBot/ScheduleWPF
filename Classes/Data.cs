773eed57c6c2bea0e31ad0b05c1928d78a0507ca
IDerkBot <ivanderk2003@gmail.com>
2022-04-08 07:45:38 +0300

    Добавил классы

--- Classes/Data.cs
+++ Classes/Data.cs
@@ -1,19 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ScheduleWPF.Classes
+﻿namespace ScheduleWPF.Classes
 {
 	internal class Data
 	{
 		public static int IDUser { get; set; }
 		public static int Access { get; set; }
 		public static bool IsStudent => Access == 0;
+		public static bool IsStaffOrAdmin => Access >= 1;
 		public static bool IsTeacher => Access == 1;
 		public static bool IsLessonWork => Access == 2;
 		public static bool IsEducationWork => Access == 3;
 		public static bool IsAdmin => Access == 4;
+		public static string CurrentDirectory { get; set; } = "ARMWPF";
+		public static string CurrentConfigFile { get; set; } = "config";
+		public static string CurrentConfigExtension { get; set; } = "json";
 	}
 }
